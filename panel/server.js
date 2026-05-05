385d2c773e9cf6c8c22cde9f71c63605b56325a0
Nikhil Tambe <134857855+nix0gg@users.noreply.github.com>
2026-05-05 11:57:12 +0530

    Implement login functionality and enhance index.html layout

---
+++ panel/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express()
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+    res.send('Server is running')
+});
+
+app.listen(3000, () => {
+    console.log('Access gateway at http://localhost:3000')
+});
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if(username==='admin' &&password ==='admin')
+    {
+        res.json({message: "Login succesful, welcome."});}
+
+        else{
+
+        
+            res.json({message: 'Incorrect credentials. Try again.'});
+        }
+
+        });
+
+    
